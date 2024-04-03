0393d06fb6e1fe5edf49a9900043bc8daa6ab00c
Thiago <thiago.larangeira50@gmail.com>
2024-04-03 10:53:26 -0300

    atividade-d

--- atividade-d/src/main/java/com/example/demo/controllers/JogadorController.java
+++ atividade-d/src/main/java/com/example/demo/controllers/JogadorController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controllers;
 
-import java.util.ArrayList;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.*;
@@ -9,12 +8,7 @@ import com.example.demo.*;
 public class JogadorController {
     @GetMapping("/GerarJogador")
     public String GerarJogador() {
-        String result = "";
-        for(int i=0; i<=10; i++){
-            result += "\n" + JogadorGerador.NovoJogador().getDescricao();
-        }
-        System.out.print(result);
-        return result;
+        return JogadorGerador.NovoJogador().getDescricao();
     }
 
     @GetMapping("/GerarJogadorJson")
